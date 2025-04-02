64ad9506c452caf5addddca646acf7735caf3fc6
Rakesh Singh <rakeshsingh432165@gmail.com>
2025-04-03 03:11:15 +0530

    api update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -20,7 +20,7 @@ function isAllowedOrigin(origin: string | null) {
     "https://www.RakeshSingh.xyz",
 
     // Include localhost for development(uncomment for development)
-    "http://localhost:3000",
+    // "http://localhost:3000",
   ];
   return origin && allowedOrigins.includes(origin);
 }
